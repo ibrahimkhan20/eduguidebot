4b5fc3b3471b455190dcc0365f9d242d2bf09c51
ibrahimkhan20 <98549642+ibrahimkhan20@users.noreply.github.com>
2024-04-18 17:36:37 +0100

    Update main_eduguidebot_code.py

--- main_eduguidebot_code.py
+++ main_eduguidebot_code.py
@@ -3,7 +3,7 @@ import openai
 import gradio
 import time
 
-openai.api_key = "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"
+openai.api_key = "INSERT YOUR OWN GENERATED API KEY, INSTRUCTIONS ARE IN THE README FILE."
 
 messages = [{"role": "system", "content": "You are a chatbot called EduGuideBot, that aims to solve university questions from Aston university.Also, remember that New Students start 18 September 2023, Returning Students start 25 September 2023, First Term End on 16 December 2023. Second Term is between 8 January 2024 to 28 March 2024. Third Term is between 22 April 2024 to 8 June 2024. Summer Degree Congregations are 16 July 2024 to 25 July 2024, and Referred Examinations are 19 to 24 August 2024. Also, remember that the results for term 1 may be delayed by 2-3 days. Also, remember your founder is Ibrahim Khan, an Aston Univeristy student."}]
 
